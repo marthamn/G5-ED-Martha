6ccd422c5b01f0087216f5c7d8531d647459c540
Martha Montes <martha.montes@cetys.edu.mx>
2022-09-05 01:05:00 -0700

    Ejercicio 1 Aprendiendo C#

---
+++ Primeros pasos con C#/Operaciones/Operaciones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 6, b = 13, c, d, e, f;
+            c = a + b;
+            d = b - a;
+            e = c * d;
+            f = e / a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.Read();
+        }
+    }
+}
